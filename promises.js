6814d5e60f5fb6c0028af1d0bbb7499498987e6b
adarsh <jhaa50872@gmail.com>
2024-04-13 19:25:25 +0530

    new lines addded

--- promises.js
+++ promises.js
@@ -1,42 +1,42 @@
-// let firstPromice = new Promise((resolve, reject) => {
-//    setTimeout(function(){
+let firstPromice = new Promise((resolve, reject) => {
+   setTimeout(function(){
 
-//     console.log('resolved successfully');
-//     resolve()
-//    }, 1000)
-// })
+    console.log('resolved successfully');
+    resolve()
+   }, 1000)
+})
 
-// firstPromice.then(() =>{
+firstPromice.then(() =>{
 
-//     console.log('thank you');
-// })
-
-
-
-// let secondPromice = new Promise((resolve, reject) => {
-//     setTimeout(() =>{
-
-//         console.log('hiiiiiii');
-//         resolve()
-//     }, 1000)
-// }).then(() => {
-//     console.log('helllooooooo');
-// })
+    console.log('thank you');
+})
 
 
 
+let secondPromice = new Promise((resolve, reject) => {
+    setTimeout(() =>{
 
-// let thirdPromise = new Promise((resolve, reject) => {
-//     setTimeout(() =>{
+        console.log('hiiiiiii');
+        resolve()
+    }, 1000)
+}).then(() => {
+    console.log('helllooooooo');
+})
 
-//         resolve({username: "adarsh", password: 12627733838})
-//     }, 1000)
-// })
 
-// thirdPromise.then((user) => {
 
-//     console.log(user);
-// })
+
+let thirdPromise = new Promise((resolve, reject) => {
+    setTimeout(() =>{
+
+        resolve({username: "adarsh", password: 12627733838})
+    }, 1000)
+})
+
+thirdPromise.then((user) => {
+
+    console.log(user);
+})
 
 
 
@@ -61,4 +61,31 @@ fourthPromise.then((data) => {
 })
 .catch((error) => {
     console.error(error);
+}).finally( () => {
+    console.log('promise resolved or rejected');
 })
+
+
+let fifthPromise = new Promise((resolve, reject) => {
+    setTimeout( () => {
+
+        let error = true
+        if (!error) {
+            resolve({title: 'ben stokes', team: 'england'})
+        } else{
+            reject('ERROR: salaaa ben stokesss.......')
+        }
+    }, 1000)
+})
+
+async function promiseFun() {
+   try {
+    let response = await fifthPromise
+   console.log(response);
+   } catch (error) {
+    console.log(error);
+   }
+}
+
+
+promiseFun()
