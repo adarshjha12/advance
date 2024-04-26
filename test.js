df8961b7f8e076505c87aecd0e6ae87fb46b054d
adarsh <jhaa50872@gmail.com>
2024-04-26 22:59:32 +0530

    line added

--- test.js
+++ test.js
@@ -7,6 +7,8 @@ const person = { name: 'John' };
 // Using call to invoke greet function with person as 'this'
 console.log(greet.call(person, 'Alice')); // Output: Hello, Alice! My name is John.
 
+
+
 function greet1 (name){
     return `Hello, ${name}! My name is ${this.name}.`;
 
@@ -15,3 +17,17 @@ function greet1 (name){
 const person1 = { name: 'John' };
 let anYarray = ['adarsh']
 console.log(greet1.apply(person, anYarray));
+
+
+
+
+function greet(name) {
+    return `Hello, ${name}! My name is ${this.name}.`;
+}
+
+const person2 = { name: 'John' };
+
+// Using bind to create a new function with person as 'this'
+const greetJohn = greet.bind(person);
+
+console.log(greetJohn('Alice')); // Output: Hello, Alice! My name is John.
